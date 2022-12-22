75e757c7d3dd850cc8ed94777c9abb63b6afcc0b
JordiPuigdo <jordipuigdollers1992@gmail.com>
2022-12-22 17:45:04 +0100

    step 5

--- Kata-StringCalc/StringCalculator.cs
+++ Kata-StringCalc/StringCalculator.cs
@@ -21,8 +21,19 @@
                 delimiters.Add(Convert.ToChar(newDelimeter));
             }
 
-            var result = numberString.Split(delimiters.ToArray())
-                    .Select(s => int.Parse(s))
+            var numberList = numberString.Split(delimiters.ToArray())
+                    .Select(s => int.Parse(s));
+
+            var negatives = numberList
+                    .Where(n => n < 0);
+
+            if(negatives.Any())
+            {
+                string negativeString = String.Join(',',negatives
+                        .Select(n => n.ToString()));
+                throw new Exception($"Negatives not allowed: {negativeString}");
+            }
+            var result = numberList
                     .Sum();
 
             return result; 
--- Kata-StringCalc/StringCalculator_Add.cs
+++ Kata-StringCalc/StringCalculator_Add.cs
@@ -17,7 +17,7 @@ namespace Kata_StringCalc
         [InlineData("1,2",3)]
         [InlineData("2,3", 5)]
         public void ReturnsSumGivenStringWithTwoCommaSeparatedNumbers( string 
-                numbers, int expectedResult)
+            numbers, int expectedResult)
         {
 
             var result = _calculator.Add(numbers);
@@ -30,7 +30,7 @@ namespace Kata_StringCalc
         [InlineData("1,2,4", 7)]
         [InlineData("2,3,4", 9)]
         public void ReturnsSumGivenStringWithThreeCommaSeparatedNumbers(string
-        numbers, int expectedResult)
+            numbers, int expectedResult)
         {
 
             var result = _calculator.Add(numbers);
@@ -44,7 +44,7 @@ namespace Kata_StringCalc
         [InlineData("1\n2\n3", 6)]
         [InlineData("1,2\n3", 6)]
         public void ReturnsSumGivenStringWithThreeCommaOrNewLineSeparatedNumbers(string
-        numbers, int expectedResult)
+            numbers, int expectedResult)
         {
 
             var result = _calculator.Add(numbers);
@@ -55,12 +55,25 @@ namespace Kata_StringCalc
         [Theory]
         [InlineData("//;\n1;2;3", 6)]       
         public void ReturnsSumGivenStringWithCostumDelimeter(string
-        numbers, int expectedResult)
+            numbers, int expectedResult)
         {
 
             var result = _calculator.Add(numbers);
 
             Assert.Equal(expectedResult, result);
+        }
+
+        [Theory]
+        [InlineData("-1,2", "Negative is not allowed : -1")]
+        [InlineData("-1,-2", "Negative is not allowed : -1, -2")]
+        public void ThrowsGivenNegativeInputs(string
+            numbers, string expectedMessage)
+        {
+            Action action = () => _calculator.Add(numbers);
+
+            var ex = Assert.Throws<Exception>(action);
+
+            Assert.Equal(expectedMessage, ex.Message);
         }
 
 
